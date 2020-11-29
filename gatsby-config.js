a1cebb88c6174293b5aebdd5ddf9e1cfc86051e2
shyal <shyalb@gmail.com>
2020-11-29 14:25:14 +0800

    change author

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Shyal Beardsley`,
+      summary: `https://shyal.com`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
